20e96ecc96d9b00ac5e88ff8b9fc7507ca6c4c62
Carlos Andé <carlosa.rebocas@gmail.com>
2021-10-26 10:05:58 -0300

    Include mores images

--- src/assets/components/NossasEstampas/index.js
+++ src/assets/components/NossasEstampas/index.js
@@ -7,28 +7,28 @@ import miniImg1 from '../../images/estampas/mini-img1.png'
 import miniImg2 from '../../images/estampas/mini-img2.png'
 import miniImg3 from '../../images/estampas/mini-img3.png'
 import miniImg4 from '../../images/estampas/mini-img4.png'
-// import miniImg5 from '../../images/estampas/mini-img5.png'
-// import miniImg6 from '../../images/estampas/mini-img6.png'
-// import miniImg7 from '../../images/estampas/mini-img7.png'
-// import miniImg8 from '../../images/estampas/mini-img8.png'
-// import miniImg9 from '../../images/estampas/mini-img9.png'
-// import miniImg10 from '../../images/estampas/mini-img10.png'
-// import miniImg11 from '../../images/estampas/mini-img11.png'
-// import miniImg12 from '../../images/estampas/mini-img12.png'
+import miniImg5 from '../../images/estampas/mini-img5.png'
+import miniImg6 from '../../images/estampas/mini-img6.png'
+import miniImg7 from '../../images/estampas/mini-img7.png'
+import miniImg8 from '../../images/estampas/mini-img8.png'
+import miniImg9 from '../../images/estampas/mini-img9.png'
+import miniImg10 from '../../images/estampas/mini-img10.png'
+import miniImg11 from '../../images/estampas/mini-img11.png'
+import miniImg12 from '../../images/estampas/mini-img12.png'
 
 // Imagens maiores
 import img1 from '../../images/estampas/img1.jpg'
 import img2 from '../../images/estampas/img2.jpg'
 import img3 from '../../images/estampas/img3.jpg'
 import img4 from '../../images/estampas/img4.jpg'
-// import img5 from '../../images/estampas/img5.png'
-// import img6 from '../../images/estampas/img6.png'
-// import img7 from '../../images/estampas/img7.png'
-// import img8 from '../../images/estampas/img8.png'
-// import img9 from '../../images/estampas/img9.png'
-// import img10 from '../../images/estampas/img10.png'
-// import img11 from '../../images/estampas/img111png'
-// import img12 from '../../images/estampas/img1.12ng'
+import img5 from '../../images/estampas/img5.jpg'
+import img6 from '../../images/estampas/img6.jpg'
+import img7 from '../../images/estampas/img7.jpg'
+import img8 from '../../images/estampas/img8.jpg'
+import img9 from '../../images/estampas/img9.jpg'
+import img10 from '../../images/estampas/img10.jpg'
+import img11 from '../../images/estampas/img11.jpg'
+import img12 from '../../images/estampas/img12.jpg'
 
 import estampasExclusivas from '../../svg/estampas-exclusivas.svg'
 import nossasEstampas from '../../svg/nossas-estampas.svg'
@@ -77,41 +77,40 @@ export default function NossasEstampas () {
 
                         <div className="rows">
                             <Imagem 
-                                miniImg={ miniImg1 }
-                                zoomImg={ img1 }
+                                miniImg={ miniImg5 }
+                                zoomImg={ img5 }
                             />
                             <Imagem 
-                                miniImg={ miniImg2 }
-                                zoomImg={ img2 }
+                                miniImg={ miniImg6 }
+                                zoomImg={ img6 }
                             />
                             <Imagem 
-                                miniImg={ miniImg3 }
-                                zoomImg={ img3 }
+                                miniImg={ miniImg7 }
+                                zoomImg={ img7 }
                             />
                             <Imagem 
-                                miniImg={ miniImg4 }
-                                zoomImg={ img4 }
+                                miniImg={ miniImg8 }
+                                zoomImg={ img8 }
                                 alt={ 'Estampa feita com a maioria dos elementos desenhados a mão.' }
                             />
                         </div>
                         
                         <div className="rows">
                             <Imagem 
-                                miniImg={ miniImg1 }
-                                zoomImg={ img1 }
+                                miniImg={ miniImg9 }
+                                zoomImg={ img9 }
                             />
                             <Imagem 
-                                miniImg={ miniImg2 }
-                                zoomImg={ img2 }
+                                miniImg={ miniImg10 }
+                                zoomImg={ img10 }
                             />
                             <Imagem 
-                                miniImg={ miniImg3 }
-                                zoomImg={ img3 }
+                                miniImg={ miniImg11 }
+                                zoomImg={ img11 }
                             />
                             <Imagem 
-                                miniImg={ miniImg4 }
-                                zoomImg={ img4 }
-                                alt={ 'Estampa feita com a maioria dos elementos desenhados a mão.' }
+                                miniImg={ miniImg12 }
+                                zoomImg={ img12 }
                             />
                         </div>
                     </div>
