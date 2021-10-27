7f908b3f9bc0243b4d643ca3d757bf2b4691e81b
Carlos Andé <carlosa.rebocas@gmail.com>
2021-10-27 11:04:10 -0300

    Correção de responsividade

--- src/assets/components/Nav/index.js
+++ src/assets/components/Nav/index.js
@@ -1,36 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-scroll';
 
 import './styles.css'
 
 export default function Nav() {
-    const [color, setColor] = useState('transparent')
-
-    window.onscroll = () => {
-        if ( window.screen.width < 600 ) {
-            document.addEventListener('scroll', () => {
-                if (document.documentElement.scrollTop > 150) {
-                    setColor('#fffdf0d8')
-
-                } else {
-                    setColor('transparent')
-                }
-            })
-        } else {
-            setColor('#fffdf0d8')
-        }
-
-    }
-
     return (
         <>
             <header id='menu'
                 className="nav"
-                style={{
-                    backgroundColor: color,
-                    zIndex: 3,
-                    transition: 'background-color 0.5s'
-                }}
             >
                 <nav className='max-container' >
                     <Link
