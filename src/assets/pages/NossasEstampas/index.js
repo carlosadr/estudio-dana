15acb7346f9e16f83cf5489530254a861e7d24cd
Carlos André <carlosa.reboucas@gmail.com>
2021-11-22 22:07:22 -0300

    Add img

--- src/assets/pages/NossasEstampas/index.js
+++ src/assets/pages/NossasEstampas/index.js
@@ -5,6 +5,7 @@ import './styles.css'
 // Imagens em miniatura
 import miniImg1 from '../../images/estampas/mini-img1.webp'
 import miniImg2 from '../../images/estampas/mini-img2.webp'
+import miniImg3 from '../../images/estampas/mini-img3.webp'
 import miniImg5 from '../../images/estampas/mini-img5.webp'
 import miniImg6 from '../../images/estampas/mini-img6.webp'
 import miniImg7 from '../../images/estampas/mini-img7.webp'
@@ -14,6 +15,7 @@ import miniImg10 from '../../images/estampas/mini-img10.webp'
 // Imagens maiores
 import img1 from '../../images/estampas/img1.webp'
 import img2 from '../../images/estampas/img2.webp'
+import img3 from '../../images/estampas/img3.webp'
 import img5 from '../../images/estampas/img5.webp'
 import img6 from '../../images/estampas/img6.webp'
 import img7 from '../../images/estampas/img7.webp'
@@ -63,6 +65,11 @@ export default function NossasEstampas() {
                         <Imagem
                             miniImg={miniImg2}
                             zoomImg={img2}
+                            alt='Estampa dente geometrica dente de leão.'
+                        />
+                        <Imagem
+                            miniImg={miniImg3}
+                            zoomImg={img3}
                             alt='Estampa dente geometrica dente de leão.'
                         />
                         <Imagem
